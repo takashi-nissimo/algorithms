0b4094f30c40f697f41bd58a12f1d815980a7111
Takashi NISHIMOTO <takashi-nissimo@email.plala.or.jp>
2023-02-25 21:17:40 +0900

    Add QueueWith2Stacks()

--- 13StacksAndQueues/README.md
+++ 13StacksAndQueues/README.md
@@ -4,5 +4,6 @@
 |file|algorithm|
 |---|---|
 |stack.py|Stack|
+|queue_with2stacks.py|Queue with two stacks|
 |deque.py|Deque (Double-Ended Queue)|
 |randomized_queue.py|Randomized Queue|
---
+++ 13StacksAndQueues/queue_with2stacks.py
@@ -0,0 +1,36 @@
+"""Queue with two stacks. Implement a queue with two stacks so that
+each queue operations takes a constant amortized number of stack operations."""
+
+
+class Stack(list):
+    """use list as stack"""
+    push = list.append
+
+
+class QueueWith2Stacks:
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+
+    def enqueue(self, x):
+        while self.s1:
+            self.s2.push(self.s1.pop())
+
+        self.s1.push(x)
+
+        while self.s2:
+            self.s1.push(self.s2.pop())
+
+    def dequeue(self):
+        if not self.s1:
+            print("Q is Empty")
+
+        return self.s1.pop()
+
+
+if __name__ == '__main__':
+    queue = QueueWith2Stacks()
+    items = (1, .5, 'abc', [1, 2, 3], None)
+    print(f'{items = }')
+    [queue.enqueue(i) for i in items]
+    [print(f'{queue.dequeue() = }') for _ in items]
