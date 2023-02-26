ff8c93a0a3edcfeab4aec7df51d5b5c917cf9dec
Takashi NISHIMOTO <takashi-nissimo@email.plala.or.jp>
2023-02-26 15:40:43 +0900

    Update randomized queue

--- 13StacksAndQueues/randomized_queue.py
+++ 13StacksAndQueues/randomized_queue.py
@@ -1,62 +1,71 @@
-from typing import Generic, TypeVar
+from typing import TypeVar
 from collections.abc import Iterable, Iterator
+from random import randrange, choice, shuffle
 
 
 class RandomizedQueue(Iterable[(Item := TypeVar('Item'))]):
 
     class RandomizedQueueIterator(Iterator[Item]):
-        def __init__(self, rand_queue):
-            pass
+        def __init__(self, randq: Iterable[Item]):
+            shuffle(randq)
+            self.randq = randq
 
         def has_next(self):
-            pass
+            return self.randq
 
         def __next__(self):
             if not self.has_next():
                 raise Exception('NoSuchElementException')
-            pass
+            return self.randq.pop()
 
         def remove(self):
             raise Exception('UnsupportedOperationException')
 
     def __init__(self):
         """construct an empty randomized queue"""
-        pass
+        self.queue = []
 
     def is_empty(self) -> bool:
         """is the randomized queue empty?"""
-        pass
+        return not self.queue
 
     def size(self) -> int:
         """return the number of items on the randomized queue"""
-        pass
+        return len(self.queue)
 
     def enqueue(self, item: Item):
         """add the item"""
-        pass
+        if item is None:
+            raise TypeError('NullPointerException')
+        self.queue.append(item)
 
     def dequeue(self) -> Item:
         """remove and return a random item"""
-        pass
+        if self.is_empty():
+            raise Exception('NoSuchElementException')
+        return self.queue.pop(randrange(len(self.queue)))
 
     def sample(self) -> Item:
         """return a random item (but do not remove it)"""
-        pass
+        if self.is_empty():
+            raise Exception('NoSuchElementException')
+        return choice(self.queue)
 
     def __iter__(self) -> Iterator[Item]:
         """return an iterator over items in order from front to back"""
-        return self.RandomizedQueueIterator(self)
+        return self.RandomizedQueueIterator(self.queue)
 
 
 if __name__ == '__main__':
     rand_queue = RandomizedQueue()
-    [rand_queue.enqueue(i) for i in range(10)]
-    print(f'{rand_queue.size() = }')
-    print(f'{rand_queue.sample() = }')
-    print(f'{rand_queue.size() = }')
-    print(f'{rand_queue.dequeue() = }')
-    print(f'{rand_queue.size() = }')
+    items = list(range(6))
+    [rand_queue.enqueue(i) for i in items]
+    print(f'{items = }')
+    print(f'{rand_queue.sample() = },\t{rand_queue.size() = }')
+    print(f'{rand_queue.sample() = },\t{rand_queue.size() = }')
+    print(f'{rand_queue.dequeue() = },\t{rand_queue.size() = }')
+    print(f'{rand_queue.dequeue() = },\t{rand_queue.size() = }')
     try:
-        [print(f'{i = }') for i in rand_queue]
+        [print(f'{item = }') for item in rand_queue]
     except Exception:
         pass
